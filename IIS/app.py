8c5e21b2fd42d47f8136729ebc0c649392bb4ee3
Daniel Paul <daniel@paul.sk>
2020-11-27 02:02:33 +0100

    Half of the model and half of the view implement...

--- IIS/app.py
+++ IIS/app.py
@@ -17,8 +17,9 @@ db.init_app(app)
 login_manager.init_app(app)
 app.cli.add_command(create_tables)
 app.cli.add_command(destroy_tables)
-from models import User
 
+from models import User,Festival,Reservation,Ticket,Stage,Interpret,perfs
+#fill_database()
 
 
 login_manager.login_view = 'app.login'
@@ -26,10 +27,28 @@ login_manager.login_view = 'app.login'
 def load_user(user_id):
     return User.query.get(user_id)
 
+
+def invalid_priviliges_check(p):
+    if(current_user.priviliges < p ):
+        return redirect(url_for('home'))
+
+def fill_database():
+    db.session.add(User(name = "aN", surname = "aS", login = "admin", unhashed_password= "admin", priviliges = 3))
+    db.session.add(User(name = "User's_Name", surname = "User's_Surname", login = "user", unhashed_password= "user", priviliges = 0))
+    db.session.add(Festival(name="Pohoda", description="Je to super festival",genre="rock",date_start="2020-12-03",date_end="2020-12-04",location="Trenčín",price="30",max_capacity="500",remaining_capacity="500"))
+    db.session.add(Interpret(name="Eminem",members="Eminem",rating="10",genre="rap"))
+    db.session.commit()
+''' HOME '''
+
 @app.route("/")
 def home():
-    #all_u = User.query.all()
-    return render_template("base.html")
+    #print(perfs)
+    festivals = Festival.query.all()
+    stages = Stage.query.all()
+    return render_template("home.html", isHome=True, current_user = current_user, festivals=festivals, stages=stages)
+
+
+''' AUTHENTIFICATION '''
 
 @app.route('/register', methods=['GET','POST'])
 def register():
@@ -42,13 +61,15 @@ def register():
         password = request.form["password"]
         name = request.form["name"]
         surname = request.form["surname"]
+        if (login == "" or password == ""):
+            return render_template("register.html", isReg= True)
         user = User(name = name, surname = surname, login = login, unhashed_password= password, priviliges = 0)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
     else:
         #Render register.html
-        return render_template("register.html")
+        return render_template("register.html", isReg=True)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -68,7 +89,7 @@ def login():
             login_user(user)
             return redirect(url_for('home'))
         
-    return render_template("login.html")
+    return render_template("login.html", isLogin= True)
 
 @app.route('/logout')
 @login_required
@@ -76,13 +97,182 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+''' PROFILE '''
+
+@app.route('/profile/<login>', methods=['GET','POST'])
+@login_required
+def profile(login):
+    user = User.query.filter_by(login=login).first()
+    if request.method == "POST":
+         return redirect(url_for('edit_profile'))
+    return render_template("profile.html", isProfile=True, current_user = current_user, viewed_user = user)
+
+@app.route('/profile/<login>/edit', methods=['GET','POST'])
+@login_required
+def edit_profile(login):
+    if(login != current_user.login):
+        return redirect(url_for('profile', login = login))
+    if request.method == "POST":
+        user = User.query.filter_by(login = login).first()
+        if (request.form["name"] != ""):
+            user.name = request.form["name"]
+        if (request.form["surname"] != ""):
+            user.surname = request.form["surname"]
+        if (request.form["login"] != ""):
+            user.login = request.form["login"]
+        db.session.commit()
+        return redirect(url_for('profile', login = current_user.login))
+    return render_template("profile.html", isProfile=True, current_user = current_user, isEdit=True)
+
+'''ROLES'''
+
+''' Admin '''
+
 @app.route('/admin')
 @login_required
 def admin():
-    if(current_user.priviliges != 1):
-        return redirect(url_for('home'))
-    return render_template("admin.html")
+    invalid_priviliges_check(3)
+    users = User.query.all()
+    return render_template("admin.html", users = users, isAdmin=True)
 
+@app.route('/remove_user/<id>')
+@login_required
+def remove_user(id):
+    invalid_priviliges_check(3)
+    user = User.query.filter_by(id=int(id)).first()
+    if user.priviliges < 3:
+        db.session.delete(user)
+        db.session.commit()
+    return redirect(url_for('admin'))
+
+''' Organizer '''
+
+@app.route('/organizer')
+@login_required
+def organizer():
+    invalid_priviliges_check(2)
+    festivals = Festival.query.all()
+    interprets = Interpret.query.all()
+    stages = Stage.query.all()
+
+    return render_template("organizer.html", festivals=festivals, interprets = interprets, stages=stages, isOrganizer=True)
+
+@app.route('/add_festival', methods=['GET','POST'])
+@login_required
+def add_festival():
+    invalid_priviliges_check(2)
+
+    if request.method == "POST":
+        if (request.form["name"] != ""):
+            name = request.form["name"]
+        if (request.form["description"] != ""):
+            description = request.form["description"]
+        if (request.form["genre"] != ""):
+            genre = request.form["genre"]
+        if (request.form["date_start"] != ""):
+            date_start = request.form["date_start"]
+        if (request.form["date_end"] != ""):
+            date_end = request.form["date_end"]
+        if (request.form["location"] != ""):
+            location = request.form["location"]
+        if (request.form["price"] != ""):
+            price = request.form["price"]
+        if (request.form["max_capacity"] != ""):
+            max_capacity = request.form["max_capacity"]
+        
+        festival = Festival(name=name, description=description,genre=genre,date_start=date_start,date_end=date_end,location=location,price=price,max_capacity=max_capacity,remaining_capacity=max_capacity)
+        db.session.add(festival)
+        db.session.commit()
+        return redirect(url_for('organizer'))
+    return render_template("add_festival.html")
+
+@app.route('/remove_festival/<id>')
+@login_required
+def remove_festival(id):
+    invalid_priviliges_check(2)
+    festival = Festival.query.filter_by(id=int(id)).first()
+    db.session.delete(festival)
+    db.session.commit()
+    return redirect(url_for('organizer'))
+
+
+@app.route('/add_stage', methods=['GET','POST'])
+@login_required
+def add_stage():
+    invalid_priviliges_check(2)
+    festivals = Festival.query.all()
+    if request.method == "POST":
+        if (request.form["name"] != ""):
+            name = request.form["name"]
+        if (request.form["festival"] != ""):
+            festival_id = request.form["festival"]
+        fest = Festival.query.filter_by(id = int(festival_id)).first()
+        stage = Stage(name=name, festival=fest)
+        db.session.add(stage)
+        db.session.commit()
+        redirect(url_for('organizer'))
+    return render_template("add_stage.html", festivals=festivals)
+
+@app.route('/remove_stage/<id>')
+@login_required
+def remove_stage(id):
+    invalid_priviliges_check(2)
+    stage = Stage.query.filter_by(id=int(id)).first()
+    db.session.delete(stage)
+    db.session.commit()
+    return redirect(url_for('organizer'))
+
+
+@app.route('/add_interpret', methods=['GET','POST'])
+@login_required
+def add_interpret():
+    invalid_priviliges_check(2)
+
+    if request.method == "POST":
+        if (request.form["name"] != ""):
+            name = request.form["name"]
+        if (request.form["members"] != ""):
+            members = request.form["members"]
+        if (request.form["rating"] != ""):
+            rating = request.form["rating"]
+        if (request.form["genre"] != ""):
+            genre = request.form["genre"]
+   
+        interpret = Interpret(name=name, members=members, rating=rating, genre=genre)
+        db.session.add(interpret)
+        db.session.commit()
+    return render_template("add_interpret.html")
+
+@app.route('/remove_interpret/<id>')
+@login_required
+def remove_interpret(id):
+    invalid_priviliges_check(2)
+    interpret = Interpret.query.filter_by(id=int(id)).first()
+    db.session.delete(interpret)
+    db.session.commit()
+    return redirect(url_for('organizer'))
+
+@app.route('/add_interpret_to_stage',  methods=['GET','POST'])
+@login_required
+def add_interpret_to_stage():
+    invalid_priviliges_check(2)
+    stages = Stage.query.all()
+    interprets = Interpret.query.all()
+    if request.method == "POST":
+        if (request.form["stage"] != ""):
+            stage_id = request.form["stage"]
+        if (request.form["interpret"] != ""):
+            interpret_id = request.form["interpret"]
+        stg = Stage.query.filter_by(id = int(stage_id)).first()
+        interp = Interpret.query.filter_by(id = int(interpret_id)).first()
+        if(interp not in stg.performers):
+            stg.performers.append(interp)
+            db.session.commit()
+        else:
+            return render_template("add_interpret_to_stage.html", stages=stages, interprets=interprets, contains=True )
+    return render_template("add_interpret_to_stage.html", stages=stages, interprets=interprets )
+
+        
 
 
 if __name__ == "__main__":
--- IIS/models.py
+++ IIS/models.py
@@ -2,13 +2,20 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash
 from extensions import db
 
+perfs= db.Table('perfs',
+    db.Column('interpret_id', db.Integer, db.ForeignKey('interpret.id')),
+    db.Column('stage_id', db.Integer, db.ForeignKey('stage.id'))
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(60))
     surname = db.Column(db.String(60))
-    login = db.Column(db.String(20))
+    login = db.Column(db.String(20), unique = True)
     password = db.Column(db.String(100))
+    # 0 - user, 1 - Cashier, 2 - Organizer, 3 - Admin
     priviliges = db.Column(db.Integer)
+    reservations = db.relationship('Reservation', backref='owner')
 
     @property
     def unhashed_password(self):
@@ -18,4 +25,44 @@ class User(UserMixin, db.Model):
     def unhashed_password(self, unhashed_password):
         self.password = generate_password_hash(unhashed_password)
     
+class Reservation(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    tickets_R = db.relationship('Ticket', backref='reservation')
+
+class Ticket(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    reservation_id = db.Column(db.Integer, db.ForeignKey('reservation.id'))
+    festival_id = db.Column(db.Integer, db.ForeignKey('festival.id'))
+
+
+class Festival(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(60), unique = True)
+    description = db.Column(db.String(150))
+    genre = db.Column(db.String(30))
+    date_start = db.Column(db.String(10))
+    date_end = db.Column(db.String(10))
+    #Rozpis
+    location = db.Column(db.String(40))
+    price = db.Column(db.Integer)
+    max_capacity = db.Column(db.Integer)
+    remaining_capacity = db.Column(db.Integer)
+    tickets_F = db.relationship('Ticket', backref='festival')
+    stages = db.relationship('Stage', backref='festival')
+
+
+class Stage(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(60))
+    festival_id = db.Column(db.Integer, db.ForeignKey('festival.id'))
+
+class Interpret(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(60), unique = True)
+    #Logo
+    members = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    genre = db.Column(db.String(30))
+    stages = db.relationship('Stage', secondary=perfs, backref=db.backref('performers', lazy='dynamic'))
 
